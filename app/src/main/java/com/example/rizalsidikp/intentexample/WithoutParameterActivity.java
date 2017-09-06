8f5dd033e888299275509586add8846cd88487c1
Rizal Sidik Permana <rizalsidikp24@gmail.com>
2017-09-06 12:51:53 +0700

    add form without param activity

---
+++ app/src/main/java/com/example/rizalsidikp/intentexample/WithoutParameterActivity.java
@@ -0,0 +1,17 @@
+package com.example.rizalsidikp.intentexample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by rizalsidikp on 06/09/17.
+ */
+
+public class WithoutParameterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.without_param_screen);
+    }
+}
